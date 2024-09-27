caa58468f39fce575956f8c88c572cd75909383e
Rostislav Raykov <rostislav.raykov@infinno.eu>
2024-09-27 19:16:45 +0300

    fixed the classname for the articles in the repository

--- internal/articles/articleRepository.go
+++ internal/articles/articleRepository.go
@@ -2,19 +2,19 @@ package articles
 
 import "NewsAggregator/database"
 
-func insertArticle(article *ArticleDB) error {
+func insertArticle(article *Article) error {
 	_, err := database.DB.NamedExec("INSERT INTO articles (title, content, source_id, source_name, author, description, url, url_to_image, published_at, created_at) VALUES (:title, :content, :source_id, :source_name, :author, :description, :url, :url_to_image, :published_at, :created_at)", article)
 	return err
 }
 
-func getAllArticles() ([]ArticleDB, error) {
-	var articles []ArticleDB
+func getAllArticles() ([]Article, error) {
+	var articles []Article
 	err := database.DB.Select(&articles, "SELECT * FROM articles")
 	return articles, err
 }
 
-func getArticleById(id int) (ArticleDB, error) {
-	var article ArticleDB
+func getArticleById(id int) (Article, error) {
+	var article Article
 	err := database.DB.Get(&article, "SELECT * FROM articles WHERE id = ?", id)
 	return article, err
 }
